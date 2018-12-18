ed1de0e598c696c44e983316aeeed23171e051ba
Luiz Santos <luiz.santos89@yahoo.com.br>
2018-12-18 10:13:26 -0200

    Início do Desenvolvimento

---
+++ View/index.php
@@ -0,0 +1,127 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../imagens/imbel.ico">
+
+    <title>Avaliação de Desempenho - DVRH/FJF</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../estilos/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="../estilos/css/pricing.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
+      <!--<h5 class="my-0 mr-md-auto font-weight-normal">Company name</h5>-->
+      <img src="../imagens/logo2.png" class="my-0 mr-md-auto font-weight-normal" />
+      
+      <nav class="my-2 my-md-0 mr-md-3">
+        <a class="p-2 text-dark" href="#">Funcionários</a>
+        <a class="p-2 text-dark" href="#">Sanções</a>
+        <a class="p-2 text-dark" href="#">Desempenho</a>
+        <a class="p-2 text-dark" href="#">Absenteísmo</a>
+        <a class="p-2 text-dark" href="#">Relatórios</a>
+      </nav>
+      <a class="btn btn-outline-primary" href="#">Sair</a>
+    </div>
+
+    <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+      <h1 class="display-4">Bem vindo [USUARIO]</h1>
+      <p class="lead">
+          Quickly build an effective pricing table for your potential customers with this Bootstrap example. It's built with default Bootstrap components and utilities with little customization.
+      </p>
+    </div>
+
+    <div class="container">
+      <div class="card-deck mb-3 text-center">
+        <div class="card mb-4 box-shadow">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Funcionários</h4>
+          </div>
+          <div class="card-body">
+            <img src="../imagens/func.png" />
+            
+            <a class="btn btn-lg btn-block btn-outline-primary">Acessar</a>
+          </div>
+        </div>
+        <div class="card mb-4 box-shadow">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Absenteísmo</h4>
+          </div>
+          <div class="card-body">
+              <img src="../imagens/hr_abs.jpg" />
+            
+            <button type="button" class="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
+          </div>
+        </div>
+        <div class="card mb-4 box-shadow">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Desempenho</h4>
+          </div>
+          <div class="card-body">
+              <img src="../imagens/desemp.png" />
+            
+            <button type="button" class="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
+          </div>
+        </div>
+      </div>
+
+      <footer class="pt-4 my-md-5 pt-md-5 border-top">
+        <div class="row">
+            <div class="col-12 col-md">
+                <img class="mb-2" src="../imagens/imbel.ico" alt="">
+                <small class="d-block mb-3 text-muted">&copy; 2018</small>
+            </div>
+          <div class="col-6 col-md">
+            <h5>Lançamentos</h5>
+            <ul class="list-unstyled text-small">
+              <li><a class="text-muted" href="#">Funcionários</a></li>
+              <li><a class="text-muted" href="#">Sanções</a></li>
+              <li><a class="text-muted" href="#">Desempenho</a></li>
+              <li><a class="text-muted" href="#">Absenteísmo</a></li>
+            </ul>
+          </div>
+          <div class="col-6 col-md">
+            <h5>Relatórios</h5>
+            <ul class="list-unstyled text-small">
+              <li><a class="text-muted" href="#">Funcionários</a></li>
+              <li><a class="text-muted" href="#">Horas de Absenteísmo</a></li>
+              <li><a class="text-muted" href="#">Notas de Desempenho</a></li>
+              <li><a class="text-muted" href="#">Aproveitamento</a></li>
+            </ul>
+          </div>
+          <div class="col-6 col-md">
+            <h5>Normas</h5>
+            <ul class="list-unstyled text-small">
+              <li><a class="text-muted" href="#">2.A.12.N-001-Rev.00</a></li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </div>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="../../assets/js/vendor/popper.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <script src="../../assets/js/vendor/holder.min.js"></script>
+    <script>
+      Holder.addTheme('thumb', {
+        bg: '#55595c',
+        fg: '#eceeef',
+        text: 'Thumbnail'
+      });
+    </script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/imagens/imbel.ico">
+
+    <title>Avaliação de Desempenho</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="estilos/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="estilos/css/signin.css" rel="stylesheet">
+  </head>
+
+  <body class="text-center">
+    <form class="form-signin">
+        <img class="mb-4" src="imagens/logo1.png" alt="" height="106">
+      <h1 class="h3 mb-3 font-weight-normal">Acessar</h1>
+      <label class="sr-only">Login</label>
+      <input type="text" id="inputEmail" class="form-control" placeholder="Login" required autofocus>
+      <label for="inputPassword" class="sr-only">Senha</label>
+      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
+      <p class="mt-5 mb-3 text-muted">
+          Divisão de Recursos Humanos <br />
+          Seção de Administração de Pessoal<br /> 
+          Indústria de Material Bélico do Brasil<br />
+          Fábrica Juiz de Fora<br />
+      </p> 
+    </form>
+  </body>
+</html>
